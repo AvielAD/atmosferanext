6a2c3f29208b2a4d207a2bb2626a5eb89ff96036
Aviel Aldama <aviel.aldama@gmail.com>
2023-12-16 10:47:13 -0600

    remove acciones codigo descuento

--- src/app/eventos/codigodescuento/page.tsx
+++ src/app/eventos/codigodescuento/page.tsx
@@ -6,7 +6,6 @@ import { useRouter } from 'next/navigation'
 import { codigodescuento, codigodescuentoupdate } from '@/DTOS/codigo/codigo.dto'
 
 const fetcher = (url: string) => fetch(url).then(r => r.json())
-const deletefetcher = async (url: string) => fetch(url, { method: "DELETE" }).then(r => r.json())
 
 const Codigo = () => {
   const router = useRouter()
@@ -22,11 +21,9 @@ const Codigo = () => {
       <table className="table w-lg-75">
         <thead>
           <tr>
-            <th scope="col">#</th>
             <th scope="col">Nombre</th>
             <th scope="col">Codigo</th>
             <th scope="col">Descuento</th>
-            <th scope="col">Acciones</th>
           </tr>
         </thead>
         <tbody>
@@ -35,15 +32,9 @@ const Codigo = () => {
             data.map((item: codigodescuentoupdate, index: number) => {
               return (
                 <tr key={item.id}>
-                  <th scope="row">{index + 1}</th>
                   <td>{item.nombre}</td>
                   <td>{item.codigo}</td>
                   <td>{item.descuento}</td>
-                  <td>
-                    <i onClick={
-                      () => deletefetcher(`/api/curso?id=${item.id}`)
-                    } className='m-2 bi bi-trash'></i>
-                  </td>
                 </tr>
               )
             })

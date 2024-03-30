a3975b50c4b60e20fb1b47fe222dbd95112be047
Aviel Aldama <aviel.aldama@gmail.com>
2024-03-30 00:33:19 -0600

    modal size

--- src/Components/ModalGeneral/page.tsx
+++ src/Components/ModalGeneral/page.tsx
@@ -6,7 +6,7 @@ const Page = (props: dataFormsModalDto) => {
             <div className={`${props.showModal ? 'd-block' : 'd-none'}`}>
                 <div onClick={() => props.close()} style={{ height: '100vh', backgroundColor: 'rgba(128,128,128,0.5)' }}
                     className="w-100 z-1 position-absolute" >
-                    <div className="d-flex h-75 justify-content-center align-items-center overflow-auto">
+                    <div className="d-flex h-100 justify-content-center align-items-center overflow-auto">
                         <div onClick={(e) => e.stopPropagation()} className="w-75 h-100 bg-primary rounded-3">
                             <div className="d-flex justify-content-center">
                                 <div className="w-75">
